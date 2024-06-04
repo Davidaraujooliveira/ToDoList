8b9b42e9e7de04ef1534e0ec8c4a10cddd7a82e4
Davidaraujooliveira <76564583+Davidaraujooliveira@users.noreply.github.com>
2024-06-04 19:08:47 +0300

    add new variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+
+function ToDoApp() { 
+  const [toDoItems, setToDoItems] = React.useState([]);
+
+  const handleAddToDoItem = (item) => { 
+    setToDoItems([...toDoItems, item]);
+  };
+
+  return (
+    <div>
+      <h1>ToDo List</h1>
+      <ToDoList items={toDoItems} />
+      <AddToDoItem onAdd={handleAddToDoItem} />
+    </div>
+  );
+}
+
+export default ToDoApp;
+
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ToDoApp from './ToDoApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
